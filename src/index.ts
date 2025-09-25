b42447a648747417bbcf7fb791cd566147cc26ea
Valentin Degenne <vdegenne@gmail.com>
2025-09-25 17:11:47 +0200

    0.1.9

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import cjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import {type RollupOptions} from 'rollup';
 
-export {typescript, nodeResolve, terser, cjs};
+export {typescript, nodeResolve, terser, cjs, json};
 
 export function config(options: RollupOptions | RollupOptions[]) {
 	return options;
