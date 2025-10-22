56a856665d158c54332a2684db15c79b000bc119
Valentin Degenne <vdegenne@gmail.com>
2025-10-22 17:21:44 +0200

    0.1.13

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import typescript from '@rollup/plugin-typescript';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
-import cjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
+import {default as typescript} from '@rollup/plugin-typescript';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import {default as terser} from '@rollup/plugin-terser';
+import {default as cjs} from '@rollup/plugin-commonjs';
+import {default as json} from '@rollup/plugin-json';
 import {type RollupOptions} from 'rollup';
 
 export {typescript, nodeResolve, terser, cjs, json};
